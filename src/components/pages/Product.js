f91017d1a9306207bae3b02a7eebe2c8e40397e3
= <=>
2025-03-27 10:58:43 +0530

    enhanced prdouct component

--- src/components/pages/Product.js
+++ src/components/pages/Product.js
@@ -5,29 +5,40 @@ import { Button, Card } from "react-bootstrap";
 const Product = () => {
   return (
     <div>
-      <h1 style={{backgroundColor: 'rgb(123, 78, 90)', marginTop: '5vh' }}>Our Products</h1>
-      <div className="products">
-        {/* map - to iterate over products data */}
-        {products.map((product) => (
-          <div>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
+      <h1 style={{ backgroundColor: "rgb(123, 78, 90)", marginTop: "5vh" }}>
+        Our Products
+      </h1>
+      <div className="container">
+        <div className="products row">
+          {/* map - to iterate over products data */}
+          {products.map((product) => (
+            <div className="col-md-3 mt-3" key={product.id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={product.image_url} />
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>
+                    Price:{" "}
+                    <span style={{ color: "darkmagenta" }}>
+                      {product.price}
+                    </span>{" "}
+                    <br />
+                    Stock:{" "}
+                    {product.stock > 0 ? (
+                      <span style={{ color: "green" }}>{product.stock}</span>
+                    ) : (
+                      <span style={{ color: "red" }}>Out of Stock</span>
+                    )}
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
-
-
 
 export default Product;
--- src/models/products.js
+++ src/models/products.js
@@ -1,23 +1,24 @@
 const products = [
     {
         "id": 1,
-        "name": "product 1",
+        "name": "Lenovo Monitor",
         "price": 1000,
         "stock": 10,
+        "desc": "Lenovo L-Series 55.88 cm (22 inch) Full HD LED Backlit IPS Panel with 99% sRGB, HDMI 1.4, VGA, 2X3W Speakers,Tilt Stand, for Home & Office use, Ultraslim Monitor (L22i-40)  (AMD Free Sync, Response Time: 4 ms, 75 Hz Refresh Rate)",
         "category": "category 1",
         "createdAt": "2021-08-01T00:00:00.000Z",
         "updatedAt": "2021-08-01T00:00:00.000Z",
-        "image_url": "https://www.google.com"
+        "image_url": "https://rukminim2.flixcart.com/image/832/832/xif0q/monitor/6/o/k/-original-imah9hc5md7furca.jpeg?q=70&crop=false"
     },
     {
         "id": 2,
         "name": "product 2",
         "price": 2000,
-        "stock": 20,
+        "stock": 0,
         "category": "category 2",
         "createdAt": "2021-08-01T00:00:00.000Z",
         "updatedAt": "2021-08-01T00:00:00.000Z",
-        "image_url": "https://www.google.com"
+        "image_url": "https://rukminim2.flixcart.com/image/832/832/kxnl6kw0/monitor/d/g/t/vz279heg1r-full-hd-27-90lm05t1-b01e10-asus-original-imaga25utdafpent.jpeg?q=70&crop=false"
     },
     {
         "id": 3,
@@ -27,7 +28,7 @@ const products = [
         "category": "category 3",
         "createdAt": "2021-08-01T00:00:00.000Z",
         "updatedAt": "2021-08-01T00:00:00.000Z",
-        "image_url": "https://www.google.com"
+        "image_url": "https://rukminim2.flixcart.com/image/832/832/xif0q/coffee/0/h/f/150-brewing-beauty-collagen-coffee-with-marine-collagen-peptide-original-imaha66fwt8hjdhq.jpeg?q=70&crop=false"
     },
     {
         "id": 4,
@@ -37,7 +38,7 @@ const products = [
         "category": "category 4",
         "createdAt": "2021-08-01T00:00:00.000Z",
         "updatedAt": "2021-08-01T00:00:00.000Z",
-        "image_url": "https://www.google.com"
+        "image_url": "https://rukminim2.flixcart.com/image/832/832/xif0q/smartwatch/i/4/b/-original-imah2thy4cphhcbe.jpeg?q=70&crop=false"
     },
     {
         "id": 5,
@@ -47,7 +48,17 @@ const products = [
         "category": "category 5",
         "createdAt": "2021-08-01T00:00:00.000Z",
         "updatedAt": "2021-08-01T00:00:00.000Z",
-        "image_url": "https://www.google.com"
+        "image_url": "https://rukminim2.flixcart.com/image/832/832/xif0q/dress/8/f/c/s-aa-0158maroon-pleated-neack-dress-aayu-original-imagg7h5ynthgcay.jpeg?q=70&crop=false"
+    },
+    {
+        "id": 6,
+        "name": "product 6",
+        "price": 6000,
+        "stock": 60,
+        "category": "category 6",
+        "createdAt": "2021-08-01T00:00:00.000Z",
+        "updatedAt": "2021-08-01T00:00:00.000Z",
+        "image_url": "https://rukminim2.flixcart.com/image/832/832/xif0q/electric-cycle/q/h/d/grande20-ebony-black-20-waya-single-speed-original-imagybpwbjvgewpr.jpeg?q=70&crop=false"
     }
 ]
 
